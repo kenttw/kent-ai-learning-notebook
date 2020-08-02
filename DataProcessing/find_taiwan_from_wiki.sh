5daf22896499ad3e09dbbe713378ddc9a482d62f
Kent Shih <kent@csie.io>
2020-08-02 16:29:26 +0800

    data clean

--- DataProcessing/find_taiwan_from_wiki.sh
+++ DataProcessing/find_taiwan_from_wiki.sh
@@ -1,1 +1,10 @@
-find  t*.json  | xargs -n1 -P4   jq .en_description |grep taiwan  | wc -l | gnomon
+#doitlive speed: 3
+cd /Users/kent/git/aiacademy-learning-notebook/DataProcessing/data/data_processing
+ls -alh titl*
+cat title000000000000.json | more
+find title* | xargs cat | wc -l
+find titl* | xargs jq . | more
+find titl* | xargs jq .en_description | more
+find titl* | xargs jq .en_description | grep taiwan | more
+find titl* | xargs -n1 -P4 jq .en_description | grep taiwan | more
+find titl* | xargs -n1 -P4 jq .en_description | grep taiwan | wc -l
