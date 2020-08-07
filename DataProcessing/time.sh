977561dea590eede3c40810b5c1cb6ccaaec4c4c
Kent Shih <kent@csie.io>
2020-08-07 21:07:37 +0800

    data clean

---
+++ DataProcessing/time.sh
@@ -0,0 +1,5 @@
+curl http://worldtimeapi.org/api/timezone/Asia/Taipei
+curl http://worldtimeapi.org/api/timezone/Asia/Taipei|jq
+curl http://worldtimeapi.org/api/timezone/Asia/Taipei|jq .datetime
+curl http://worldtimeapi.org/api/timezone/Asia/Taipei|jq .timezone
+
